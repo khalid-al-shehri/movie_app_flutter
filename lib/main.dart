a2d22b13d60a67e6a1174ea24fae07acc2c6a35f
khalid-al-shehri <k.shehri@outlook.com>
2021-04-04 23:45:20 +0300

    BuildProviders for some blocs

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app_sdaia/bloc/casts_bloc/casts_bloc.dart';
+import 'package:movie_app_sdaia/bloc/casts_bloc/casts_state.dart';
 import 'package:movie_app_sdaia/bloc/genre_bloc/genre_bloc.dart';
 import 'package:movie_app_sdaia/bloc/movie_by_genre_bloc/movie_by_genre_bloc.dart';
 import 'package:movie_app_sdaia/bloc/movie_by_genre_bloc/movie_by_genre_state.dart';
+import 'package:movie_app_sdaia/bloc/movie_details_bloc/movie_details_bloc.dart';
+import 'package:movie_app_sdaia/bloc/movie_details_bloc/movie_details_state.dart';
+import 'package:movie_app_sdaia/bloc/top_rated_movies/top_rated_movies_bloc.dart';
+import 'package:movie_app_sdaia/bloc/top_rated_movies/top_rated_movies_state.dart';
+import 'package:movie_app_sdaia/bloc/trending_persons_bloc/trending_persons_bloc.dart';
+import 'package:movie_app_sdaia/bloc/trending_persons_bloc/trending_persons_state.dart';
 import 'package:movie_app_sdaia/repository/repository.dart';
-import 'package:movie_app_sdaia/screen/mainScreen.dart';
-import 'package:movie_app_sdaia/widgets/searchField.dart';
-
+import 'package:movie_app_sdaia/screen/main_screen.dart';
 import 'bloc/genre_bloc/genre_state.dart';
 
 void main() {
@@ -15,8 +21,10 @@ void main() {
       debugShowCheckedModeBanner: false,
       home: MultiBlocProvider(
         providers: [
-          BlocProvider<GenreBloc>( create: (context) => GenreBloc(GenreInitialState(), Repository())),
-          BlocProvider<MovieByGenreBloc>( create: (context) => MovieByGenreBloc(MovieByGenreInitialState(), Repository())),
+          BlocProvider<GenreBloc>(create: (context) => GenreBloc(GenreInitialState(), Repository())),
+          BlocProvider<MovieByGenreBloc>(create: (context) => MovieByGenreBloc(MovieByGenreInitialState(), Repository())),
+          BlocProvider<TrendingPersonsBloc>(create: (context) => TrendingPersonsBloc(TrendingPersonsInitialState(), Repository())),
+          BlocProvider<TopRatedMoviesBloc>(create: (context) => TopRatedMoviesBloc(TopRatedMoviesInitialState(), Repository())),
         ],
         child: MainScreen(),
       ),
