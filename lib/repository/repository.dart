0e4c4dc8e5391ac10043ef702e5f80b25d177b7b
khalid-al-shehri <k.shehri@outlook.com>
2021-04-04 23:41:26 +0300

    methods to fetch data from APIs

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -1,20 +1,25 @@
-
 import 'dart:convert';
-
-import 'package:movie_app_sdaia/model/genreModel.dart';
+import 'package:movie_app_sdaia/model/casts_model.dart';
+import 'package:movie_app_sdaia/model/genre_model.dart';
 import 'package:http/http.dart' as http;
-import 'package:movie_app_sdaia/model/movieModel.dart';
+import 'package:movie_app_sdaia/model/movie_details_model.dart';
+import 'package:movie_app_sdaia/model/movie_model.dart';
+import 'package:movie_app_sdaia/model/trending_persons_model.dart';
+import 'package:connectivity/connectivity.dart';
+import 'package:movie_app_sdaia/model/video_model.dart';
 
 class Repository{
 
-  static String apiKey = "8a1227b5735a7322c4a43a461953d4ff";
+  static String apiKey = "9f11e1d3bb79deff6ae49d6475b1896e";
   static String baseUrl = "api.themoviedb.org";
-  var getPopularUrl = '/3/movie/top_rated';
+  var getTopRatedMoviesUrl = '/3/movie/top_rated';
   var getMoviesUrl = '/3/discover/movie';
   var getPlayingUrl = '/3/movie/now_playing';
   var getGenresUrl = "/3/genre/movie/list";
-  var getPersonsUrl = "/3/trending/person/week";
+  var getTrendingPersonsUrl = "/3/trending/person/week";
+  var getSearchUrl = "/3/search/movie";
   var movieUrl = "/3/movie";
+
 
   Future<List<GenreModel>> getGenre() async{
 
@@ -40,13 +45,124 @@ class Repository{
         movieByGenre.add(MovieModel.fromJson(movie));
       }).toList();
 
-      print(movieByGenre.length);
-
       return movieByGenre;
     }
 
   }
 
+  Future<List<MovieModel>> search(String searchValue) async{
 
+    List<MovieModel> moviesSearch = [];
+
+    var response = await http.get(Uri.https(baseUrl, getSearchUrl, {"api_key": apiKey, "language": "en-US", "query": searchValue.toString()}));
+    if(response.statusCode == 200){
+      var data = json.decode(response.body)['results'];
+      print(data);
+      data.map((movie){
+        moviesSearch.add(MovieModel.fromJson(movie));
+      }).toList();
+
+      return moviesSearch;
+    }
+
+  }
+
+  Future<List<TrendingPersonsModel>> getTrendingPersons() async{
+
+    List<TrendingPersonsModel> trendingPersons = [];
+
+    var response = await http.get(Uri.https(baseUrl, getTrendingPersonsUrl, {"api_key": apiKey}));
+    if(response.statusCode == 200){
+      var data = json.decode(response.body)['results'];
+      data.map((movie){
+        trendingPersons.add(TrendingPersonsModel.fromJson(movie));
+      }).toList();
+
+      return trendingPersons;
+    }
+
+  }
+
+  Future<List<MovieModel>> getTopRatedMovies() async{
+
+    List<MovieModel> topRatedMovies = [];
+
+    var response = await http.get(Uri.https(baseUrl, getTopRatedMoviesUrl, {"api_key": apiKey, "language": "en-US", "page": "1"}));
+    if(response.statusCode == 200){
+      var data = json.decode(response.body)['results'];
+      data.map((movie){
+        topRatedMovies.add(MovieModel.fromJson(movie));
+      }).toList();
+
+      return topRatedMovies;
+    }
+
+  }
+
+  Future<List<MovieDetailsModel>> getMovieDetails(int movieID) async{
+
+    List<MovieDetailsModel> movieInfo = [];
+
+    var response = await http.get(Uri.https(baseUrl, movieUrl+"/${movieID.toString()}", {"api_key": apiKey, "language": "en-US"}));
+    if(response.statusCode == 200){
+      List dataInList = [];
+      var data = json.decode(response.body);
+      dataInList.add(data);
+      dataInList.map((movie){
+        movieInfo.add(MovieDetailsModel.fromJson(movie));
+      }).toList();
+
+      return movieInfo;
+    }
+
+  }
+
+  Future<List<CastsModel>> getCasts(int movieID) async{
+
+    List<CastsModel> casts = [];
+
+    var response = await http.get(Uri.https(baseUrl, movieUrl+"/${movieID.toString()}"+"/credits", {"api_key": apiKey, "language": "en-US"}));
+    if(response.statusCode == 200){
+      var data = json.decode(response.body)['cast'];
+      data.map((cast){
+        casts.add(CastsModel.fromJson(cast));
+      }).toList();
+
+      return casts;
+    }
+
+  }
+
+  Future<List<MovieModel>> getSimilarMovies(int movieID) async{
+
+    List<MovieModel> similarMovies = [];
+
+    var response = await http.get(Uri.https(baseUrl, movieUrl+"/${movieID.toString()}"+"/similar", {"api_key": apiKey, "language": "en-US"}));
+    if(response.statusCode == 200){
+      var data = json.decode(response.body)['results'];
+      data.map((movie){
+        similarMovies.add(MovieModel.fromJson(movie));
+      }).toList();
+
+      return similarMovies;
+    }
+
+  }
+
+  Future<List<VideoModel>> getVideo(int movieID) async{
+
+    List<VideoModel> movieViedos = [];
+
+    var response = await http.get(Uri.https(baseUrl, movieUrl+"/${movieID.toString()}"+"/videos", {"api_key": apiKey, "language": "en-US"}));
+    if(response.statusCode == 200){
+      var data = json.decode(response.body)['results'];
+      data.map((video){
+        movieViedos.add(VideoModel.fromJson(video));
+      }).toList();
+
+      return movieViedos;
+    }
+
+  }
 
 }
